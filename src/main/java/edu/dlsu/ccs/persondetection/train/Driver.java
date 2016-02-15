674557db9ae944fec8976cf3041fe58d726f2184
John <john_raphael_aquino@dlsu.edu.ph>
2016-02-15 21:23:40 +0800

    Recompiled opencv; fixed StatModel.save. Refactored training code; implemented hard training

--- src/main/java/edu/dlsu/ccs/persondetection/train/Driver.java
+++ src/main/java/edu/dlsu/ccs/persondetection/train/Driver.java
@@ -1,95 +1,18 @@
 package edu.dlsu.ccs.persondetection.train;
 
-import static org.bytedeco.javacpp.opencv_imgcodecs.imread;
-
-import java.io.File;
-
-import org.bytedeco.javacpp.FloatPointer;
-import org.bytedeco.javacpp.opencv_core;
-import org.bytedeco.javacpp.opencv_core.Mat;
-import org.bytedeco.javacpp.opencv_core.PointVector;
-import org.bytedeco.javacpp.opencv_core.Scalar;
-import org.bytedeco.javacpp.opencv_core.Size;
-import org.bytedeco.javacpp.opencv_ml;
-import org.bytedeco.javacpp.opencv_ml.SVM;
-import org.bytedeco.javacpp.opencv_objdetect.HOGDescriptor;
+import org.opencv.core.Core;
 
 public class Driver {
 
 	public static void main(String[] args) throws Exception {
-		// System.loadLibrary(opencv_core.Core.NATIVE_LIBRARY_NAME);
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 
-		File positivesDirectory = new File("train/positives");
-		File negativesDirectory = new File("train/negatives");
-		if (!positivesDirectory.isDirectory()) {
-			throw new RuntimeException("Directory for positive images is missing");
-		}
-		if (!negativesDirectory.isDirectory()) {
-			throw new RuntimeException("Directory for negative images is missing");
-		}
-
-		Mat trainData = new Mat();
-		Mat responses = new Mat();
-//		HOGDescriptor hog = new HOGDescriptor(new Size(64, 128), new Size(8, 8),
-//				new Size(8, 8), new Size(4, 4), 9);
-		HOGDescriptor hog = new HOGDescriptor();
-		for (String filename : positivesDirectory.list()) {
-			FloatPointer descriptor = new FloatPointer();
-			hog.compute(imread(positivesDirectory.getPath() + "\\" + filename),
-					descriptor, new Size(8, 8), new Size(0, 0), new PointVector());
-			Mat matDescriptor = new Mat(descriptor);
-			Mat reshapedDescriptor = matDescriptor.reshape(0, 1);
-			matDescriptor.close();
-			trainData.push_back(reshapedDescriptor);
-			responses.push_back(new Mat(new Size(1, 1), opencv_core.CV_32SC1, new Scalar(1.0)));
-			
-		}
-
-		for (String filename : negativesDirectory.list()) {
-			FloatPointer descriptor = new FloatPointer();
-			hog.compute(imread(negativesDirectory.getPath() + "\\" + filename), descriptor, new Size(8, 8),
-					new Size(0, 0), new PointVector());
-			Mat matDescriptor = new Mat(descriptor);
-			Mat reshapedDescriptor = matDescriptor.reshape(0, 1);
-			matDescriptor.close();
-			trainData.push_back(reshapedDescriptor);
-			responses.push_back(new Mat(new Size(1, 1), opencv_core.CV_32SC1, new Scalar(-1.0)));
-		}
-		SVM svm = SVM.create();
-		svm.setC(100);
-		svm.setKernel(SVM.LINEAR);
-
-		svm.train(trainData, opencv_ml.ROW_SAMPLE, responses);
-
-		File testPositivesDirectory = new File("test/positives");
-		File testNegativesDirectory = new File("test/negatives");
-
-		System.out.println("testing");
-		System.out.println("positives");
-		for (String filename : testPositivesDirectory.list()) {
-			FloatPointer descriptors = new FloatPointer();
-			hog.compute(imread(testPositivesDirectory.getPath() + "\\" + filename), descriptors, new Size(8, 8),
-					new Size(0, 0), new PointVector());
-			Mat matDescriptor = new Mat(descriptors);
-			Mat reshapedDescriptor = matDescriptor.reshape(0, 1);
-			matDescriptor.close();
-			System.out.println(svm.predict(reshapedDescriptor));
-		}
-
-		System.out.println("negatives");
-		for (String filename : testNegativesDirectory.list()) {
-			FloatPointer descriptors = new FloatPointer();
-			hog.compute(imread(testNegativesDirectory.getPath() + "\\" + filename), descriptors, new Size(8, 8),
-					new Size(0, 0), new PointVector());
-			Mat matDescriptor = new Mat(descriptors);
-			Mat reshapedDescriptor = matDescriptor.reshape(0, 1);
-			matDescriptor.close();
-			System.out.println(svm.predict(reshapedDescriptor));
-		}
-		hog.close();
-		svm.save("svm_persondetect.xml");
-		// HOGDescriptor humanDetector = new HOGDescriptor();
-		// humanDetector.setSVMDetector(svm.getSupportVectors());
+		SVMTrainer trainer = new SVMTrainer();
+		trainer.start("data/training/positives", 
+						"data/training/negatives", 
+						"data/testing/positives", 
+						"data/testing/negatives", 
+						"data/svm_persondetect.xml");
 	}
 
 }
---
+++ src/main/java/edu/dlsu/ccs/persondetection/train/SVMTrainer.java
@@ -0,0 +1,149 @@
+package edu.dlsu.ccs.persondetection.train;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfFloat;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.ml.Ml;
+import org.opencv.ml.SVM;
+import org.opencv.objdetect.HOGDescriptor;
+
+public class SVMTrainer {
+
+	private HOGDescriptor hogDescriptor;
+	private static final double POSITIVE_LABEL = 1.0;
+	private static final double NEGATIVE_LABEL = -1.0;
+	private SVM svm;
+
+	public SVMTrainer() {
+		hogDescriptor = new HOGDescriptor();
+		svm = SVM.create();
+		svm.setC(100);
+		svm.setKernel(SVM.LINEAR);
+	}
+
+	public void start(String trainingPositivesDirectoryPath, String trainingNegativesDirectoryPath,
+			String testingPositivesDirectoryPath, String testingNegativesDirectoryPath, String filename)
+					throws Exception {
+		String[] currentFalseNegatives = new String[0];
+		String[] currentFalsePositives = new String[0];
+		List<String> falseNegatives = new ArrayList<>();
+		List<String> falsePositives = new ArrayList<>();
+		do{
+			Mat trainData = new Mat();
+			Mat responses = new Mat();
+			if(currentFalseNegatives.length > 0 || currentFalsePositives.length > 0){
+				System.out.println("Retraining");
+			}
+			else {
+				System.out.println("Training");
+			}
+			
+			trainFromDirectory(trainingPositivesDirectoryPath, trainingNegativesDirectoryPath,
+					trainData, responses);
+			
+
+			System.out.println("Training from list");
+			System.out.println("Training false negatives");
+			trainFromList(falseNegatives, trainData, responses, 1.0);
+			System.out.println("Training false positives");
+			trainFromList(falsePositives, trainData, responses, -1.0);
+
+			svm.train(trainData, Ml.ROW_SAMPLE, responses);
+			
+			String[][] testResult = test(testingPositivesDirectoryPath, testingNegativesDirectoryPath);
+			currentFalseNegatives = testResult[0];
+			currentFalsePositives = testResult[1];
+			falseNegatives.addAll(Arrays.asList(currentFalseNegatives));
+			falsePositives.addAll(Arrays.asList(currentFalsePositives));
+		} while(currentFalseNegatives.length > 0 || currentFalsePositives.length > 0);
+		save(filename);
+	}
+
+	private void trainFromDirectory(String positivesDirectoryPath, String negativesDirectoryPath,
+			Mat trainData, Mat responses) throws Exception {
+		File positivesDirectory = new File(positivesDirectoryPath);
+		File negativesDirectory = new File(negativesDirectoryPath);
+
+		System.out.println("Training from directories");
+		System.out.println("Training positives");
+		getDescriptorsForTrainingSet(positivesDirectory, trainData, responses, POSITIVE_LABEL);
+		System.out.println("Training negatives");
+		getDescriptorsForTrainingSet(negativesDirectory, trainData, responses, NEGATIVE_LABEL);
+	}
+
+	private void trainFromList(List<String> trainingDataPaths, Mat trainData, Mat responses, double classLabel)
+			throws Exception {
+		System.out.println(trainingDataPaths.size() + " training data from list");
+		for (String filePath : trainingDataPaths) {
+			Mat imageDescriptor = getDescriptorForImage(filePath);
+			pushTrainDataAndResponse(imageDescriptor, trainData, responses, classLabel);
+		}
+	}
+
+	private void getDescriptorsForTrainingSet(File trainingDataDirectory, Mat trainData, Mat responses,
+			double classLabel) throws Exception {
+		System.out.println(trainingDataDirectory.list().length + " training data from directory");
+		for (String filename : trainingDataDirectory.list()) {
+			Mat imageDescriptor = getDescriptorForImage(trainingDataDirectory.getPath() + "\\" + filename);
+			pushTrainDataAndResponse(imageDescriptor, trainData, responses, classLabel);
+		}
+	}
+	
+	private void pushTrainDataAndResponse(Mat imageDescriptor, Mat trainData, Mat responses, double classLabel){
+		trainData.push_back(imageDescriptor);
+		responses.push_back(new Mat(new Size(1, 1), CvType.CV_32SC1, new Scalar(classLabel)));
+	}
+
+	// get hog descriptor of gray-scale version of image specified by filePath
+	private Mat getDescriptorForImage(String filePath) throws Exception {
+		MatOfFloat imageDescriptor = new MatOfFloat();
+		Mat image = Imgcodecs.imread(filePath);
+		Mat grayScale = new Mat();
+		Imgproc.cvtColor(image, grayScale, Imgproc.COLOR_BGR2GRAY);
+		hogDescriptor.compute(grayScale, imageDescriptor);
+		Mat reshapedImageDescriptor = imageDescriptor.reshape(0, 1);
+		return reshapedImageDescriptor;
+	}
+
+	private String[][] test(String positivesDirectoryPath, String negativesDirectoryPath) throws Exception {
+		System.out.println("testing SVM");
+		File testPositivesDirectory = new File(positivesDirectoryPath);
+		File testNegativesDirectory = new File(negativesDirectoryPath);
+		System.out.println("testing positive set");
+		String[] falseNegatives = testDataSet(testPositivesDirectory, POSITIVE_LABEL);
+		System.out.println(falseNegatives.length + " false negatives");
+		System.out.println("testing negative set");
+		String[] falsePositives = testDataSet(testNegativesDirectory, NEGATIVE_LABEL);
+		System.out.println(falsePositives.length + " false positives");
+		return new String[][]{falseNegatives, falsePositives};
+	}
+
+	private String[] testDataSet(File testDataDirectory, double expectedClassLabel) throws Exception {
+		List<String> errors = new ArrayList<>();
+		for (String filename : testDataDirectory.list()) {
+			String filePath = testDataDirectory.getPath() + "\\" + filename;
+			Mat imageDescriptor = getDescriptorForImage(filePath);
+			float actualClassLabel = svm.predict(imageDescriptor);
+			if (actualClassLabel != (float) expectedClassLabel) {
+//				System.out.println("Error on " + filename + "; " + "expected: " + expectedClassLabel + " actual: "
+//						+ actualClassLabel);
+//				System.out.println("Adding to re-training set");
+				errors.add(filePath);
+			}
+		}
+		return errors.toArray(new String[errors.size()]);
+	}
+
+	private void save(String filename) {
+		svm.save("svm_persondetect.xml");
+	}
+}
